889b3b3caf0870a951ba260d702f2cc9cd81835d
arthurfcarvalho <satierfruhtra@protonmail.com>
2024-09-15 13:28:31 -0300

    dasdasd

--- src/app/features/chatbot/pages/chat/chat.component.ts
+++ src/app/features/chatbot/pages/chat/chat.component.ts
@@ -15,7 +15,7 @@ import { ChatService } from '../../../../core/services/chat.service';
 })
 export class ChatComponent {
   userInput: {message: string} = {message: ''};
-  messages: {text: string, sender: string, options?: string[], color?: string}[] = [];
+  messages: {text: string, sender: string, options?: string[], color?: string, slider?: boolean, min?: number, max?: number, value?: number}[] = [];
   selectedOption: string = '';
   data: {grupo?: string, message: string} = {message: '', grupo: 'desenvolvedor web'};
 
@@ -27,15 +27,18 @@ export class ChatComponent {
     this.messages.push({text: this.userInput.message, sender: 'Você'});
     this.data.message = this.userInput.message;
     this.chatService.test(this.data).subscribe((response) => {
-      if(response.habilidades_descritas){
+      if(response.habilidades_descritas.length > 0){
         this.messages.push({text: 'Habilidades do colaborador: ' + response.habilidades_descritas, sender: 'Chatbot', color: 'green'});
       }
-      if(response.habilidades_faltantes){
+      if(response.habilidades_faltantes.length > 0){
         this.messages.push({text: 'Habilidades que faltam ao colaborador: ' + response.habilidades_faltantes, sender: 'Chatbot', color: 'red'});
       }
-      if(response.habilidades_relacionadas){
+      if(response.habilidades_relacionadas.length > 0){
         this.messages.push({text: 'Habilidades que podem estar relacionadas baseada em outros conhecimentos do colaborador: ' + response.habilidades_relacionadas, sender: 'Chatbot', color: 'orange'});
       }
+
+      this.messages.push({text: 'Quantidade de competências: ', sender: 'Chatbot', slider: true, min: 0, max: response.habilidades_descritas + response.habilidades_faltantes, value: response.habilidades_descritas.length});
+      
     });
 
     this.userInput.message = '';
